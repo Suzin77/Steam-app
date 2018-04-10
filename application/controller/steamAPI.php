e2d1339bf31bc2be0cb3779ab6ad67112f76fc3e
Patrycjusz Sosnowski <patrycjusz.sosnowski@gmail.com>
2018-04-10 12:46:32 +0200

    formating
    formating

--- application/controller/steamAPI.php
+++ application/controller/steamAPI.php
@@ -5,10 +5,8 @@ class SteamAPI extends Controller
     public function index()
     {
         /* Page: index.
-        */
-        //$steamUserModel = $this->loadModel('SteamUsersModel');
-        echo "jestesmy w klasie ".get_class($this);
-        //header('location: ' . URL . 'steamAPI/search');
+        */        
+        header('location: ' . URL . 'steamAPI/search');
     }
 
     public function deleteUser($user_id)
@@ -35,33 +33,13 @@ class SteamAPI extends Controller
         $exampleToCheck = $DataSearchReadModel->getRandomRows('steam_id','steam_users_to_check',10);	
         if(isset($_POST['submit_search_steam_user'])){			
         //sanitization of enterned data.
-            $_POST['steam_user_id'] = $steamApiModel->sanitizeString(($_POST['steam_user_id']));
-            $userInfo = $SteamAPISearchReadModel->searchSteamUser($_POST['steam_user_id']);
-            $userFriends = $SteamAPISearchReadModel->getSteamUserFriends($_POST['steam_user_id']);
-            $userGames = $SteamAPISearchReadModel->getSteamUserGames($_POST['steam_user_id']);
-            /*
-            if($userGames['response']){
-                foreach($userGames['response']['games'] as $game =>$id){
-                    if($DataSearchReadModel->isGame($userGames['response']['games'][$game]['appid'])){
-                    $gameData = $userModel->getSteamGameData($userGames['response']['games'][$game]['appid']); 
-                        if($gameData[$userGames['response']['games'][$game]['appid']]['success'] == true){
-                            $DataSearchWriteModel->writeGame($userGames['response']['games'][$game]['appid'],$gameData);
-                        }
-                    }					
-                }
-                //$user_model->writeUserGamesRealtion($userID, $userGames['response']['games']);
-            }
-            */
-            /*if($user_model->checkUserGames($userID, 240)){
-                echo "trzeba zapiac bo nie ma </br>";
-            } else {
-                echo "było";
-            }*/            
-            //$userArray = $user_model->recursiveResponse($userinfo);
-            //var_dump($user_model->checkUser(76561197997461962));
-            if ($DataSearchReadModel->checkUser($_POST['steam_user_id'])){
+            $_POST['clean_steam_user_id'] = $steamApiModel->sanitizeString(($_POST['steam_user_id']));
+            $userInfo = $SteamAPISearchReadModel->searchSteamUser($_POST['clean_steam_user_id']);
+            $userFriends = $SteamAPISearchReadModel->getSteamUserFriends($_POST['clean_steam_user_id']);
+            $userGames = $SteamAPISearchReadModel->getSteamUserGames($_POST['CLEAN_steam_user_id']);
+                     
+            if ($DataSearchReadModel->checkUser($_POST['clean_steam_user_id'])){
                 $DataSearchWriteModel->addUser($userInfo);						
-            } else {
             }
 
             $ftable = $this->loadView('tablesviews');
@@ -104,11 +82,10 @@ class SteamAPI extends Controller
 			}
 			$DataSearchWriteModel->removeId($userId, "steam_users_to_check");
 		}
-		//header('refresh:5; url='.URL.'steamAPI/search');
 		header('location: '. URL . 'steamAPI/search');
 	} 
 
-	public function admin()
+    public function admin()
     {
 
     }
