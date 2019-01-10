c3c8597825252b5d2e12ec9cf3e969e4df3a1a42
Patrycjusz Sosnowski <patrycjusz.sosnowski@gmail.com>
2019-01-10 19:19:08 +0100

    test guzzle class added.

--- application/models/SteamConnect.php
+++ application/models/SteamConnect.php
@@ -4,10 +4,16 @@ use GuzzleHttp\Client;
 class SteamConnect extends Model
 {
 
-    public function connect()
+    public function startConnect()
     {
-        $connection = new Client(['base_uri'=>'http://www.wp.pl']);
-        $response = $connection->get();
+        $connection = new Client(['base_uri'=>'https://store.steampowered.com/api/appdetails?appids=3900']);
+        $response = $connection->request('GET','https://store.steampowered.com/api/appdetails?appids=3900');
+        $statusCode = $response->getStatusCode();
         return $response;
+    }
+
+    public function getBody($response)
+    {
+        return $response->getBody();
     }
 }
